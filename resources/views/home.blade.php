144049157d0576a5e58ecf66e0c4af918e789836
Jasmine Perez <jasmineperez24@gmail.com>
2015-09-06 21:29:08 -0400

    added block bodies for the cash register page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,65 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+	<div class="col">
+		<div class="col-md-8">
 			<div class="panel panel-default">
-				<div class="panel-heading">Cash Register</div>
-
+				<div class="panel-heading">Transaction Log</div>
 				<div class="panel-body">
-					You are logged in!
+                    <table class="table table-hover">
+                      <thead>
+                          <tr>
+                             <th>Item Name</th>
+                             <th>Price</th>
+                             <th>Quantity</th>
+                             <th>Total</th>
+                             <th>Edit</th>
+                             <th>Delete</th>
+                          </tr>
+                      </thead>
+
+                      <tbody>
+                        <tr class="active">
+                            <td></td>
+                            <td><strong>Total Value:</strong></td>
+                            <td><strong>Total Quantity:</strong></td>
+                            <td></td>
+                         </tr>
+                      </tbody>
+                    </table>
 				</div>
 			</div>
 		</div>
 	</div>
+
+		<div class="col">
+    		<div class="col-md-4">
+    			<div class="panel panel-default">
+
+    				@if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                        <div class="panel-heading">Inventory</div>
+                        <div class="panel-body">
+                         <table class="table table-hover">
+                          <thead>
+                              <tr>
+                                 <th>Item Name</th>
+                                 <th>Price</th>
+                                 <th>Quantity</th>
+                              </tr>
+                          </thead>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
 </div>
 @endsection
